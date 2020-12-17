872777e92b6deef71b7e16f6f5b924165c455429
sfl-jakeprim <sufulu@foxmail.com>
2020-12-17 17:06:59 +0800

    nt

--- moudle1/src/main/moudle/java/Moudle1App.java
+++
@@ -1,7 +0,0 @@
-package demo.prim.com.moudle1;
-
-import android.app.Application;
-
-public class Moudle1App extends Application {
-
-}
--- primrouter-core/src/main/java/com/primrouter_core/interfaces/IRouteRoot.java
+++
@@ -1,7 +0,0 @@
-package com.primrouter_core.interfaces;
-
-import java.util.Map;
-
-public interface IRouteRoot {
-    void loadInto(Map<String, Class<? extends IRouteGroup>> routers);
-}
